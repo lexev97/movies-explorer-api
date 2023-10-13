175545b1a391dacf15586132d5b038798f5202cb
Lexev <evtenkoaleksei@gmail.com>
2023-10-13 21:47:43 +0300

    cors correction

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const { errors } = require('celebrate');
 const helmet = require('helmet');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { corsHandler } = require('./middlewares/cors');
-// const { limiter } = require('./middlewares/limiter');
+const { limiter } = require('./middlewares/limiter');
 
 const errorHandler = require('./middlewares/errorHandler');
 const { MONGOOSE_DB } = require('./constants/constants');
@@ -14,15 +14,15 @@ const { MONGOOSE_DB } = require('./constants/constants');
 const { PORT = 5000 } = process.env;
 
 const app = express();
-app.use(corsHandler);
 mongoose.connect(MONGOOSE_DB);
 
 app.use(helmet());
+app.use(corsHandler);
 app.use(express.json());
 app.use(cookieParser());
 app.use(requestLogger);
 
-app.use('/', require('./routes/index'));
+app.use('/', limiter, require('./routes/index'));
 
 app.use(errorLogger);
 
