25d42c38e3226874938fd5a7203d0dbe7f3ba1a8
Lexev <evtenkoaleksei@gmail.com>
2023-10-13 21:09:37 +0300

    cors correction

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -11,7 +11,7 @@ const corsHandler = (req, res, next) => {
   const requestHeaders = req.headers['access-control-request-headers'];
 
   if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Credentials', true);
   }
 
